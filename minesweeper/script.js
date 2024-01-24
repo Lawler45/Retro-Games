592a8e31512351725649790c6535f5ce656ece55
Lewis Lawler <lewislawler98@gmail.com>
2024-01-24 17:23:00 +0000

    fixes deploy issues

---
+++ minesweeper/script.js
@@ -0,0 +1,114 @@
+const scoreCounter = document.querySelector(".score-counter");
+const grid = document.querySelector(".grid");
+const endGameScreen = document.querySelector(".end-game-screen");
+const endGameText = document.querySelector(".end-game-text");
+const playAgainButton = document.querySelector(".play-again");
+
+const totalCells = 100;
+const totalBombs = 20;
+const maxScore = 40;
+const bombsList = [];
+
+let score = 0;
+
+//GAME FUNCTIONS
+
+while (bombsList.length < totalBombs) {
+  const randomNumber = Math.floor(Math.random() * totalCells) + 1;
+
+  if (!bombsList.includes(randomNumber)) {
+    bombsList.push(randomNumber);
+  }
+}
+
+function updateScore() {
+  score++;
+  scoreCounter.innerText = score.toString().padStart(5, "0");
+
+  if (score === maxScore) {
+    endGame(true);
+  }
+}
+
+function handleRightClick(event, cell, index) {
+  event.preventDefault();
+  addFlag(cell);
+}
+
+function addFlag(cell) {
+  if (
+    !cell.classList.contains("cell-clicked") &&
+    !cell.classList.contains("cell-flag")
+  ) {
+    cell.classList.add("cell-flag");
+  } else if (cell.classList.contains("cell-flag")) {
+    cell.classList.remove("cell-flag");
+  }
+}
+
+function countNeighbourBombs(cellIndex) {
+  const neighbours = [-11, -10, - 9, -1, 1, 9, 10, 11];
+
+  return neighbours.filter((neighbour) =>
+    bombsList.includes(cellIndex + neighbour)
+  ).length;
+}
+
+function revealAllBombs() {
+  const cells = document.querySelectorAll(".cell");
+
+  for (let i = 1; i <= cells.length; i++) {
+    const cell = cells[i - 1];
+
+    if (bombsList.includes(i)) {
+      cell.classList.add("cell-bomb");
+    }
+  }
+}
+
+//GAME FUNCTIONAILITY
+for (let i = 1; i <= totalCells; i++) {
+  const cell = document.createElement("div");
+  cell.classList.add("cell");
+
+  cell.addEventListener("click", function () {
+    if (bombsList.includes(i)) {
+      cell.classList.add("cell-bomb");
+      endGame(false);
+    } else {
+      const neighbourBombCount = countNeighbourBombs(i);
+      if (neighbourBombCount > 0) {
+        cell.innerText = neighbourBombCount;
+        if (cell.innerText <= 2) {
+          cell.classList.add("cell-number-low");
+        } else if (cell.innerText == 3) {
+          cell.classList.add("cell-number-medium");
+        } else {
+          cell.classList.add("cell-number-high");
+        }
+      }
+    }
+    cell.classList.add("cell-clicked");
+    updateScore();
+  });
+
+  cell.addEventListener("contextmenu", function (event) {
+    handleRightClick(event, cell, i);
+  });
+
+  grid.appendChild(cell);
+}
+
+function endGame(isVictory) {
+  if (isVictory) {
+    endGameText.innerHTML = "YOU </BR> WON";
+    endGameScreen.classList.add("win");
+  }
+
+  revealAllBombs();
+  endGameScreen.classList.remove("hidden");
+}
+
+playAgainButton.addEventListener("click", function () {
+  window.location.reload();
+});
---
+++ stacker/script.js
@@ -0,0 +1,151 @@
+const grid = document.querySelector(".grid");
+const stackBtn = document.querySelector(".stack");
+const scoreCounter = document.querySelector(".score-counter");
+const endGameScreen = document.querySelector(".end-game-screen");
+const endGameText = document.querySelector(".end-game-text");
+const playAgainBtn = document.querySelector(".play-again");
+
+//create game grid
+//0 - empty cell
+//1 - bar-segment
+const gridMatrix = [
+  [0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0],
+  [1, 1, 1, 0, 0, 0], // This is our starting currentRowIndex (see below)
+];
+
+//game tracking variables
+let currentRowIndex = gridMatrix.length - 1;
+let barDirection = "right";
+let barSize = 3;
+let isGameOver = false;
+let score = 0;
+
+function draw() {
+  grid.innerHTML = "";
+
+  gridMatrix.forEach(function (rowContent) {
+    rowContent.forEach(function (cellContent) {
+      const cell = document.createElement("div");
+      cell.classList.add("cell");
+
+      if (cellContent === 1) {
+        cell.classList.add("bar");
+      }
+
+      grid.appendChild(cell);
+    });
+  });
+}
+
+// Game logic and controls
+function endGame(isVictory) {
+  if (isVictory) {
+    endGameText.innerHTML = "YOU<br>WON!";
+    endGameScreen.classList.add("win");
+  }
+
+  endGameScreen.classList.remove("hidden");
+}
+
+function checkWin() {
+  if (currentRowIndex === 0) {
+    isGameOver = true;
+    clearInterval(gameInterval);
+    endGame(true);
+  }
+}
+
+function checkLost() {
+  const currentRow = gridMatrix[currentRowIndex];
+  const prevRow = gridMatrix[currentRowIndex + 1];
+
+  if (!prevRow) return;
+
+  for (let i = 0; i < currentRow.length; i++) {
+    if (currentRow[i] === 1 && prevRow[i] === 0) {
+      currentRow[i] = 0;
+      barSize--;
+    }
+    if (barSize === 0) {
+      isGameOver = true;
+      clearInterval(gameInterval);
+      endGame(false);
+    }
+  }
+}
+
+function updateScore() {
+  score += barSize;
+  scoreCounter.innerText = score.toString().padStart(5, 0);
+}
+
+function onStack() {
+  checkWin();
+  checkLost();
+
+  if (isGameOver) return;
+
+  updateScore();
+
+  currentRowIndex--;
+  barDirection = "right";
+
+  for (let i = 0; i < barSize; i++) {
+    gridMatrix[currentRowIndex][i] = 1;
+  }
+
+  draw();
+}
+
+function moveRight(currentRow) {
+  currentRow.pop();
+  currentRow.unshift(0);
+}
+
+function moveLeft(currentRow) {
+  currentRow.shift();
+  currentRow.push(0);
+}
+
+function moveBar() {
+  const currentRow = gridMatrix[currentRowIndex];
+
+  if (barDirection === "right") {
+    moveRight(currentRow);
+
+    const lastElement = currentRow[currentRow.length - 1];
+    if (lastElement === 1) {
+      barDirection = "left";
+    }
+  } else {
+    moveLeft(currentRow);
+
+    const firstElement = currentRow[0];
+    if (firstElement === 1) {
+      barDirection = "right";
+    }
+  }
+}
+
+function onPlayAgain() {
+  location.reload();
+}
+// Initial load of screen
+draw();
+
+// Moves the bar automatically
+function main() {
+  moveBar();
+  draw();
+}
+
+// Events
+stackBtn.addEventListener("click", onStack);
+playAgainBtn.addEventListener("click", onPlayAgain);
+const gameInterval = setInterval(main, 200);
